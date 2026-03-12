0923e56ee0dda3e130df5822c19d6b22f9e86501
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-03-12 11:09:16 +0530

    (v0.0.0-alpha) Initial commit
    Just setting up gradle and stuff

---
+++ src/client/java/com/example/client/enchantedcombatClient.java
@@ -0,0 +1,10 @@
+package com.example.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class enchantedcombatClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
